7541db91cc83b7db729e4f26df5d5af9dd32e257
Antonio Obradović <55183634+AntonioObra@users.noreply.github.com>
2022-11-29 13:47:09 +0100

    fix

--- components/HomePage/AboutHome.jsx
+++ components/HomePage/AboutHome.jsx
@@ -7,11 +7,11 @@ const AboutHome = () => {
         <div className={styles.LeftSide}>
           <h2>hy! my name is antonio and welcome to my blog.</h2>
           <p>
-            this is a place where i'll be documenting my journey from becoming
-            an aspiring web developer to landing a full time job, landing
-            freelancing jobs, as well as blog tutorials on varius topics around
-            web dev. there will also be a lot of other blogs about videogames
-            that were inpactfull as well as books that i read.
+            this is a place where i&#39;ll be documenting my journey from
+            becoming an aspiring web developer to landing a full time job,
+            landing freelancing jobs, as well as blog tutorials on varius topics
+            around web dev. there will also be a lot of other blogs about
+            videogames that were inpactfull as well as books that i read.
           </p>
         </div>
         <img src="images/Resume.svg" alt="" />

eb9852154439e849233dacd37f5800a720175481
Antonio Obradović <55183634+AntonioObra@users.noreply.github.com>
2022-12-03 17:27:55 +0100

    design changes

--- components/HomePage/AboutHome.jsx
+++ components/HomePage/AboutHome.jsx
@@ -1,9 +1,25 @@
 import styles from "./AboutHome.module.scss";
 
+import Typewriter from "typewriter-effect";
+
 const AboutHome = () => {
   return (
     <div className={styles.AboutHome}>
-      <h2>čalskdjfčakljsd lčfčalkjsd</h2>
+      {/* <h2>you could be good today, but instead you choose tomorrow</h2> */}
+
+      <Typewriter
+        options={{
+          strings: [
+            "<h2>you could be good today, but instead you choose tomorrow</h2>",
+            "<h2>the person who does wrong, does wrong to themselves</h2>",
+            "<h2>better to trip with the feet than with the tounge</h2>",
+            "<h2>to stop talking about what the good man is like, and just be one</h2>",
+          ],
+          autoStart: true,
+          loop: true,
+        }}
+        style={{ color: "white" }}
+      />
     </div>
   );
 };

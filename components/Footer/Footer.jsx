b7c8737b91063ec237fd4c28248b093138321a22
Antonio Obradović <55183634+AntonioObra@users.noreply.github.com>
2022-12-10 21:11:38 +0100

    footer fix

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,4 +1,6 @@
 import styles from "./Footer.module.scss";
+import Link from "next/link";
+import Image from "next/image";
 
 const Footer = () => {
   return (
@@ -6,8 +8,18 @@ const Footer = () => {
       <div className={styles.container}>
         <div className={styles.left}>
           <h2>blog.obradovic</h2>
+          aaskd
         </div>
-        <div className={styles.right}></div>
+        <div className={styles.right}>
+          <a href="https://twitter.com/antonioobra8" target="_blank">
+            {" "}
+            <Image src="/images/twitter.svg" alt="" width={50} height={50} />
+          </a>
+
+          <a href="https://github.com/AntonioObra" target="_blank">
+            <Image src="/images/githubIcon.svg" alt="" width={50} height={50} />
+          </a>
+        </div>
       </div>
     </div>
   );

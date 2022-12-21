f5af635a83ebc3d26f378a05d2583523798c3d48
Antonio Obradović <55183634+AntonioObra@users.noreply.github.com>
2022-12-21 23:44:34 +0100

    added navbar, more components to the header

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,34 +2,34 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav className="container mx-auto text-center p-6 text-white flex items-center justify-center space-x-6">
+    <nav className="hidden container mx-auto text-center p-6 text-white md:flex items-center justify-center space-x-6">
       <Link
         href="/posts"
-        className="hover:text-sky-100 duration-150 hover:-translate-y-1"
+        className="hover:text-sky-100 duration-150 hover:-translate-y-1 text-lg"
       >
         posts
       </Link>
       <Link
         href="/photos"
-        className="hover:text-sky-100 duration-150 hover:-translate-y-1"
+        className="hover:text-sky-100 duration-150 hover:-translate-y-1 text-lg"
       >
         photos
       </Link>
       <Link
         href="/"
-        className="hover:text-sky-100 duration-150 hover:-translate-y-1"
+        className="hover:text-sky-100 duration-150 hover:-translate-y-1 text-lg"
       >
         blog.obradovic
       </Link>
       <Link
         href="/about"
-        className="hover:text-sky-100 duration-150 hover:-translate-y-1"
+        className="hover:text-sky-100 duration-150 hover:-translate-y-1 text-lg"
       >
         about
       </Link>
       <Link
         href="/contact"
-        className="hover:text-sky-100 duration-150 hover:-translate-y-1"
+        className="hover:text-sky-100 duration-150 hover:-translate-y-1 text-lg"
       >
         contact
       </Link>
--- pages/index.js
+++ pages/index.js
@@ -14,19 +14,43 @@ export default function Home() {
       <Navbar />
 
       {/* Header */}
-      <header className="container  pt-20  mx-auto flex flex-col space-y-12 justify-evenly items-center lg:flex-row md:space-y-0 ">
-        <h1 className="text-6xl md:text-8xl xl:text-9xl font-semibold text-left z-20 text-sky-100">
-          blog.<br></br>{" "}
-          <span className="decoration-8 underline decoration-sky-300 ">
-            {" "}
+      <header className="container  pt-20 mx-auto flex flex-col space-y-12 justify-evenly items-top lg:flex-row md:space-y-0 ">
+        <div className="w-2/3 mx-auto lg:w-1/2 lg:mx-0">
+          <h1 className="text-6xl md:text-8xl xl:text-9xl font-semibold text-left z-20 text-white ">
+            blog.<br></br>{" "}
+            {/* <span className="decoration-8 underline decoration-sky-300 "> */}{" "}
             obradovic
-          </span>
-        </h1>
-
-        <img
-          src="/images/blob3.svg"
-          className="hidden lg:block md:w-96 md:h-96"
-        />
+            {/* </span> */}
+          </h1>
+          <div className="border-b-2 border-sky-200 my-5"></div>
+          <p className="text-xl text-white">
+            byte of the internet for my thoughts regarding my web development
+            journey, reading and videogames
+          </p>
+          <div className="border-b-2 border-sky-200 my-5 mb-7"></div>
+          <div className="flex items-start space-x-6">
+            <a
+              target="_blank"
+              href="https://twitter.com/antonioobra8"
+              className="text-black bg-sky-200 py-5 px-7 font-bold border-2 border-sky-200 text-center hover:bg-[#0f0f0f] hover:text-sky-200 duration-150 "
+            >
+              follow me on twitter
+            </a>
+            <a
+              target="_blank"
+              href="https://github.com/AntonioObra"
+              className="text-white bg-sky-700 py-5 px-7 font-bold border-2 border-sky-700 text-center hover:bg-[#0f0f0f] hover:text-sky-700 duration-150 "
+            >
+              checkout my github
+            </a>
+          </div>
+        </div>
+        <div className="">
+          <img
+            src="/images/blob3.svg"
+            className="hidden lg:block md:w-96 md:h-96 "
+          />
+        </div>
       </header>
     </div>
   );

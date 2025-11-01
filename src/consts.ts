3916087a69dc9db98e64de9d1edbde52c89ace73
Antonio Obradovic <antonio@obradovic.dev>
2025-11-01 15:54:32 +0100

    Added: new blog

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
-export const SITE_TITLE = "blog.obradovic";
-export const SITE_DESCRIPTION = "Antonio Obradovic - Full stack devoper";
+export const SITE_TITLE = "obradovic.blog";
+export const SITE_DESCRIPTION = "Antonio Obradovic - Full stack Developer";
